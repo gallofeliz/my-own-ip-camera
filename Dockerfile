4cb57186ed2c11a2b4bef3dd580e616280a3b58a
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2022-12-31 22:40:02 +0100

    Waste time

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
 FROM node:lts-alpine
 
-RUN apk add --no-cache tzdata libcamera ffmpeg
-RUN apk add --no-cache git
+RUN apk add --no-cache tzdata ffmpeg git
 
 WORKDIR /app
 
-#ADD package.json package-lock.json ./
-#RUN npm i
-RUN npm i https://github.com/gallofeliz/js-libs
+ADD package.json package-lock.json ./
+RUN npm i
 ADD index.js ./
 
 #USER nobody
-
 
 CMD node index.js
 #CMD ls -la node_modules
--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const HttpServer = require('@gallofeliz/js-libs/http-server').default
 const createLogger = require('@gallofeliz/js-libs/logger').default
 const {handleExitSignals} = require('@gallofeliz/js-libs/exit-handle')
+const { Logger } = require('@gallofeliz/js-libs/logger')
+const runProcess = require('@gallofeliz/js-libs/process').default
+
+const logger = createLogger('info')
 
 const server = new HttpServer({
-    port: 80,
-    logger: createLogger('info'),
+    port: 8080,
+    logger,
     api: {
         routes: [
-{
-method: 'GET',
-path: '/fhd',
-handler(req, res) {
-   res.end('Hello')
-}
-}
+            {
+                method: 'GET',
+                path: '/fhd',
+                async handler(req, res) {
+                    res.header('Content-Type: image/jpeg')
+                    await runProcess({
+                        cmd: 'ffmpeg',
+                        args: ['-i', 'http://localhost:8888/fhd/stream.m3u8', '-f', 'image2', '-vframes', '1', '-'],
+                        logger,
+                        outputStream: res
+                    }, true)
+                }
+            }
         ]
     }
 })
